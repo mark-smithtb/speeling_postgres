a63d3749c070966dc4432ad0a51b43f755197e67
msmith46234 <msmith46234@yahoo.com>
2015-09-17 10:48:40 -0400

    "renamed main rb file and add add function"

--- speeling.rb
+++ speeling.rb
@@ -1,58 +1,29 @@
 require 'net/https'
-require 'webrick'
+require 'sinatra'
 require 'json'
 require 'erb'
 
-class Dictionary < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET(request, response)
-    template = File.read("page.html.erb")
-    dictionary_file = JSON.parse(File.read("dictionary_file.json"))
+get '/' do
+  @dictionary_file = JSON.parse(File.read("dictionary_file.json"))
+  erb :page
+end
 
-    response.status = 200
-    response.body = ERB.new(template).result(binding)
+get '/search' do
+  @search = params['search_word']
+  @array_of_hashes = JSON.parse(File.read("dictionary_file.json"))
+  erb :search
+end
+
+post '/save' do
+  word = params['word']
+  definition = params['definition']
+  hash = {word: word, definition: definition}
+
+  array_of_hashes = JSON.parse(File.read("dictionary_file.json"))
+  array_of_hashes << hash
+
+  File.open("dictionary_file.json", "w") do |file|
+    file.puts array_of_hashes.to_json
   end
+ redirect '/'
 end
-
-class Saveword < WEBrick::HTTPServlet::AbstractServlet
-  def do_POST(request, response)
-    word       = request.query["word"]
-    definition = request.query["definition"]
-
-    hash = {
-      :word => word,
-      :definition => definition
-    }
-
-    array_of_hashes = JSON.parse(File.read("dictionary_file.json"))
-    array_of_hashes << hash
-
-    File.open("dictionary_file.json", "w") do |file|
-      file.puts array_of_hashes.to_json
-    end
-
-    response.status = 302
-    response.header['location'] = '/'
-    response.body = 'Your word was added.'
-  end
-end
-
-class Search < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET(request, response)
-    search = request.query['search_word']
-    template = File.read("search.html.erb")
-    array_of_hashes = JSON.parse(File.read("dictionary_file.json"))
-
-    response.status = 200
-    response.body = ERB.new(template).result(binding)
-  end
-end
-
-server = WEBrick::HTTPServer.new(Port: 3000)
-server.mount '/', Dictionary
-server.mount '/save', Saveword
-server.mount '/search', Search
-trap('INT') do
-  server.shutdown
-end
-
-server.start
